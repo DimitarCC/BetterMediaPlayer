50918fc8caaac472662d30e7c2784c6f6978d2e0
Dimitar Tsenev <dimitarcenevjp@gmail.com>
2014-10-12 13:19:29 +0300

    - changed core library

--- BetterMediaPlayer/BetterMediaPlayer/MainWindow.xaml.cs
+++ BetterMediaPlayer/BetterMediaPlayer/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using com.ptrampert.LibVLCBind;
+﻿using Declarations;
+using Declarations.Media;
+using Declarations.Players;
+using Implementation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,21 +24,35 @@ namespace BetterMediaPlayer
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private IVLCInstance vlc;
-		private IMedia media;
-		private IVideoPlayer player;
+		IMediaPlayerFactory m_factory;
+		IDiskPlayer m_player;
+		IMediaFromFile m_media;
 		private const string filename = @"W:\Action\Batman (1989).mkv";
 		public MainWindow()
 		{
 			InitializeComponent();
-			IVLCFactory factory = new TwoflowerVLCFactory();
-			vlc = factory.InitializeVLC(new string[1] { "--subsdec-encoding=Windows-1251" });
-			media = vlc.GetVLCMedia(filename, true);
-			player = vlc.CreateMediaPlayer<IVideoPlayer>();
-			player.Media = media;
-			player.Drawable = this.VideoCanvas.Handle;
-			player.Mute = false;
-			player.Play();
+			m_factory = new MediaPlayerFactory(true);
+			m_player = m_factory.CreatePlayer<IDiskPlayer>();
+			m_player.WindowHandle = this.VideoCanvas.Handle;
+			
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+			if (ofd.ShowDialog() == true)
+			{
+				m_media = m_factory.CreateMedia<IMediaFromFile>(ofd.FileName);
+				//m_media.Events.DurationChanged += new EventHandler<MediaDurationChange>(Events_DurationChanged);
+				//m_media.Events.StateChanged += new EventHandler<MediaStateChange>(Events_StateChanged);
+				//m_media.Events.ParsedChanged += new EventHandler<MediaParseChange>(Events_ParsedChanged);
+
+				m_player.Open(m_media);
+				m_media.Parse(true);
+
+				m_player.Play();
+			}
 		}
 	}
 }
